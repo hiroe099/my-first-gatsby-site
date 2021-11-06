7d2184c88986be8b8c2d03fe0be66288ccf28e78
Hilo <93075597+hiroe099@users.noreply.github.com>
2021-11-06 16:50:15 +0900

    Add files via upload

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
-import React from "react"
+import * as React from 'react'
+import Layout from '../components/layout'
 
-export default function Home() {
-  return <div>Hello world!</div>
+const IndexPage = () => {
+  return (
+    <Layout pageTitle="Home Page">
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+    </Layout>
+  )
 }
+
+export default IndexPage
